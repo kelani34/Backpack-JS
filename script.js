1dced3704b1f5293064293f249488fef7ee90c34
Shina-Kelani Taiwo <58753439+kelani34@users.noreply.github.com>
2022-05-01 03:13:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Note: This file is intentionally empty.
+ * You can use it to test your skills at adding, removing, or modifying
+ * attributes, classes, and inline styles on elements.
+ */
+
+
+
+document.querySelector("img").setAttribute("class", "blackBag 1stBackpack")
